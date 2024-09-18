4a79339c892431a854b8aa2744d7371f1c400105
readra <yong6302@gmail.com>
2024-09-18 18:40:49 +0900

    1. Spring Security > 다중 인증 방식 오류 사항 구현

---
+++ src/main/java/com/toy/springsecuritymultipleauth/SpringSecurityMultipleAuthApplication.java
@@ -0,0 +1,13 @@
+package com.toy.springsecuritymultipleauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityMultipleAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityMultipleAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toy/springsecuritymultipleauth/SpringSecurityMultipleAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.springsecuritymultipleauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityMultipleAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
